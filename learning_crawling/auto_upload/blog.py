8908d94f22aa75a9dc5372ef6c99619c2097d313
gosm-7757 <gogosm7757@naver.com>
2023-09-21 23:19:47 +0900

    블로그 글 작성

--- learning_crawling/auto_upload/blog.py
+++ learning_crawling/auto_upload/blog.py
@@ -19,6 +19,7 @@ chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options) # chrome_options는 사라짐
 driver.get('https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fm.mail.naver.com%2F')
+driver.maximize_window()
 
 time.sleep(2)
 # 이렇게 입력하면 입력 속도가 너무 빨라서 내가 봇인줄 앎
@@ -26,6 +27,34 @@ time.sleep(2)
 pyperclip.copy('gogosm7757') # ctrl + c
 driver.find_element(By.CSS_SELECTOR, '#id').send_keys(Keys.CONTROL, 'v')
 time.sleep(1)
+
 pyperclip.copy('dlatnals12')
 driver.find_element(By.CSS_SELECTOR, '#pw').send_keys(Keys.CONTROL, 'v')
 driver.find_element(By.CSS_SELECTOR, '#pw').send_keys(Keys.ENTER)
+# 로그인 성공!! 캡처 우회 성공함 
+
+
+# 시간 텀을 주지 않으면 너무 빨라서 봇인줄 의심함
+# 블로그 입장
+time.sleep(2)
+driver.get('https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0')
+
+# 글쓰기 입장
+time.sleep(2)
+driver.get('https://blog.naver.com/gogosm7757?Redirect=Write')
+
+# 제목 작성
+time.sleep(2)
+e = driver.find_element(By.CSS_SELECTOR,'span.se-text-paragraph .se-ff-nanumgothic')
+e.send_keys("블로그 제목입니다.")
+
+# 내용 작성
+time.sleep(2)
+e = driver.find_element(By.CSS_SELECTOR, 'span.se-text-paragraph .se-ff-nanumgothic')
+e.send_keys("블로그의 내용입니다. \n 오늘 강남 맛집 다녀왔습니다.")
+e.send_keys(Keys.ENTER)
+e.send_keys("비쌌습니다.")
+
+
+driver.close()
+
